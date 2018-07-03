fea90fc316b98d422cab829b9398583a8f0ae5b0
Ishwer Kumar <ishwerkumar666@gmail.com>
2018-07-04 02:18:29 +0530

    Forming Magic Square -- Hardcoded

--- formingMagicSquare.py
+++ formingMagicSquare.py
@@ -1,40 +1,24 @@
 def formingMagicSquare(s):
-    m1 = [[8,3,4],[1,5,9],[6,7,2]]
-    m2 = [[4,9,2],[3,5,7],[8,1,6]]
-    m3 = [[2,7,6],[9,5,1],[4,3,8]]
-    m4 = [[6,1,8],[7,5,3],[2,9,4]]
+    m = [
+            [[8, 1, 6], [3, 5, 7], [4, 9, 2]],
+            [[6, 1, 8], [7, 5, 3], [2, 9, 4]],
+            [[4, 9, 2], [3, 5, 7], [8, 1, 6]],
+            [[2, 9, 4], [7, 5, 3], [6, 1, 8]], 
+            [[8, 3, 4], [1, 5, 9], [6, 7, 2]],
+            [[4, 3, 8], [9, 5, 1], [2, 7, 6]], 
+            [[6, 7, 2], [1, 5, 9], [8, 3, 4]], 
+            [[2, 7, 6], [9, 5, 1], [4, 3, 8]]
+        ]
     
-    c1,c2,c3,c4 = 0,0,0,0
-    
-    for i in range(3):
-        for j in range(3):
-            c1 += abs(m1[i][j] - s[i][j])
-            c2 += abs(m2[i][j] - s[i][j])
-            c3 += abs(m3[i][j] - s[i][j])
-            c4 += abs(m4[i][j] - s[i][j])
-
-    if c1 < c2:
-        if c1 < c3:
-            if c1 < c4:
-                return c1
-            else:
-                return c4
-        else:
-            if c3 < c4:
-                return c3
-            else:
-                return c4
-    else:
-        if c2 < c3:
-            if c2 < c4:
-                return c2
-            else:
-                return c4
-        else:
-            if c3 < c4:
-                return c3
-            else:
-                return c4
+    c = []
+    for l in m:
+        tot = 0
+        for i in range(3):
+            for j in range(3):
+                tot += abs(l[i][j] - s[i][j])
+        c.append(tot)
+        
+    return min(c)
 
 if __name__ == '__main__':
 
